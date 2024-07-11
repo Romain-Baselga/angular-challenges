bf50489b7983b6cef22ca3ab1899ad1a3dbb429b
Romain Baselga <romain.baselga@gmail.com>
2024-07-11 10:40:10 +0200

    feat: lazy load a component

--- apps/angular/52-lazy-load-component/src/app/app.component.ts
+++ apps/angular/52-lazy-load-component/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
+import { PlaceholderComponent } from './placeholder.component';
+import { TopComponent } from './top.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [TopComponent, PlaceholderComponent],
   template: `
     <div class="h-screen bg-gray-500">
-      @if (topLoaded()) {
+      @defer (when topLoaded()) {
         <app-top />
-      } @else {
+      } @placeholder {
         <app-placeholder />
         <button
           class="rounded-sm border border-blue-500 bg-blue-300 p-2"
