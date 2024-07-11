201415c62179a8779ea1e8d0273560c4fb8589f7
Romain Baselga <romain.baselga@gmail.com>
2024-07-11 19:25:53 +0200

    refactor: my way (don't like global css variable, because I don't like magic string)

--- apps/angular/13-highly-customizable-css/src/app/page.component.ts
+++ apps/angular/13-highly-customizable-css/src/app/page.component.ts
@@ -1,17 +1,18 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component } from '@angular/core';
 import { TextStaticComponent } from './static-text.component';
-import { TextComponent } from './text.component';
 
 @Component({
   selector: 'page',
   standalone: true,
-  imports: [TextStaticComponent, TextComponent],
+  imports: [TextStaticComponent],
   template: `
     <static-text></static-text>
     <static-text type="error"></static-text>
     <static-text type="warning"></static-text>
-    <text [font]="15" color="blue">This is a blue text</text>
+
+    <!-- This is a bad idea, a real project show not allow sutch customisation -->
+    <!-- <text [font]="15" color="blue">This is a blue text</text> -->
   `,
 })
 export class PageComponent {}
--- apps/angular/13-highly-customizable-css/src/app/static-text.component.ts
+++ apps/angular/13-highly-customizable-css/src/app/static-text.component.ts
@@ -1,33 +1,33 @@
 /* eslint-disable @angular-eslint/component-selector */
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { TextComponent } from './text.component';
 
 export type StaticTextType = 'normal' | 'warning' | 'error';
 
 @Component({
   selector: 'static-text',
   standalone: true,
-  imports: [TextComponent],
+  imports: [NgClass],
   template: `
-    <text [font]="font" [color]="color">This is a static text</text>
+    <p [ngClass]="type">This is a static text</p>
+  `,
+  styles: `
+    .normal {
+      font: 10;
+      color: black;
+    }
+
+    .error {
+      font: 30;
+      color: red;
+    }
+
+    .warning {
+      font: 25;
+      color: orange;
+    }
   `,
 })
 export class TextStaticComponent {
-  @Input() set type(type: StaticTextType) {
-    switch (type) {
-      case 'error': {
-        this.font = 30;
-        this.color = 'red';
-        break;
-      }
-      case 'warning': {
-        this.font = 25;
-        this.color = 'orange';
-        break;
-      }
-    }
-  }
-
-  font = 10;
-  color = 'black';
+  @Input() type!: StaticTextType;
 }
