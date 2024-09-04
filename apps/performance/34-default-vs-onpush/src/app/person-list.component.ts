be74c204f6a80ccae2b17d89c175a4f870721c1a
Romain Baselga <romain.baselga@gmail.com>
2024-09-04 15:54:11 +0200

    feat: optimisation 2, track on ngFor

--- apps/performance/34-default-vs-onpush/src/app/person-list.component.ts
+++ apps/performance/34-default-vs-onpush/src/app/person-list.component.ts
@@ -35,17 +35,17 @@ import { MatListModule } from '@angular/material/list';
     </mat-form-field>
 
     <mat-list class="flex w-full">
-      <div *ngIf="names?.length === 0" class="empty-list-label">Empty list</div>
-      <mat-list-item
-        *ngFor="let name of names"
-        cd-flash
-        class="text-orange-500">
-        <div MatListItemLine class="flex justify-between">
-          <h3 title="Name">
-            {{ name }}
-          </h3>
-        </div>
-      </mat-list-item>
+      @for (name of names; track name) {
+        <mat-list-item cd-flash class="text-orange-500">
+          <div MatListItemLine class="flex justify-between">
+            <h3 title="Name">
+              {{ name }}
+            </h3>
+          </div>
+        </mat-list-item>
+      } @empty {
+        <div class="empty-list-label">Empty list</div>
+      }
       <mat-divider *ngIf="names?.length !== 0"></mat-divider>
     </mat-list>
   `,
