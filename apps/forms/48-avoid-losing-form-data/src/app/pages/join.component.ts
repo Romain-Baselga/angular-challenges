eb13a3ee27967a2c7527faa139cdd2a82167691a
Romain Baselga <romain.baselga@gmail.com>
2024-09-04 15:20:00 +0200

    feat: solve challange
    matDialogClose do not work for strange reason

--- apps/forms/48-avoid-losing-form-data/src/app/pages/join.component.ts
+++ apps/forms/48-avoid-losing-form-data/src/app/pages/join.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AlertDialogComponent } from '../ui/dialog.component';
 import { FormComponent } from '../ui/form.component';
-
 @Component({
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, MatDialogModule],
   template: `
     <section class="mx-auto	max-w-screen-sm">
       <div class="rounded-lg bg-white p-8 shadow-lg lg:p-12">
@@ -13,4 +15,26 @@ import { FormComponent } from '../ui/form.component';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class JoinComponent {}
+export class JoinComponent {
+  @ViewChild(FormComponent)
+  formComponent!: FormComponent;
+
+  constructor(private dialog: Dialog) {}
+
+  canDeactivate() {
+    const atLeastOneFieldIsNotEmpty: boolean = Object.values(
+      this.formComponent.form.getRawValue(),
+    ).some((value) => value.length > 0);
+
+    if (atLeastOneFieldIsNotEmpty) {
+      const dialogRef = this.dialog.open(AlertDialogComponent, {
+        role: 'alertdialog',
+        disableClose: true,
+      });
+
+      return dialogRef.closed;
+    } else {
+      return true;
+    }
+  }
+}
--- apps/forms/48-avoid-losing-form-data/src/app/ui/dialog.component.ts
+++ apps/forms/48-avoid-losing-form-data/src/app/ui/dialog.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-// NOTE : this is just the dialog content, you need to implement dialog logic
+import { DialogRef } from '@angular/cdk/dialog';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 @Component({
   standalone: true,
@@ -14,12 +13,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
       <div class="mt-4 flex gap-2">
         <button
-          class="inline-flex items-center gap-2 rounded-lg bg-red-600 px-4 py-2 text-white hover:bg-red-700">
+          class="inline-flex items-center gap-2 rounded-lg bg-red-600 px-4 py-2 text-white hover:bg-red-700"
+          (click)="closeDialog(true)">
           Yes continue
         </button>
 
         <button
-          class="block rounded-lg px-4 py-2 text-gray-700 transition hover:bg-gray-50">
+          class="block rounded-lg px-4 py-2 text-gray-700 transition hover:bg-gray-50"
+          (click)="closeDialog(false)">
           Stay on page
         </button>
       </div>
@@ -27,4 +28,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlertDialogComponent {}
+export class AlertDialogComponent {
+  protected dialogRef = inject(DialogRef);
+
+  closeDialog(result: boolean) {
+    this.dialogRef.close(result);
+  }
+}
