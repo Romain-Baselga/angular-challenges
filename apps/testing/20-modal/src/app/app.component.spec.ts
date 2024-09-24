d2a7051ee0f4a57a15e61d5db3573098188bbc1c
Romain Baselga <romain.baselga@gmail.com>
2024-09-24 19:21:14 +0200

    feat: implmeent test

--- apps/testing/20-modal/src/app/app.component.spec.ts
+++ apps/testing/20-modal/src/app/app.component.spec.ts
@@ -1,16 +1,50 @@
-import { render } from '@testing-library/angular';
+import { fireEvent, render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  test('error modal is displayed if you click on "Confirm" without inputing a name', async () => {
+  let inputBar: HTMLElement;
+  let confirmButton: HTMLElement;
+
+  beforeEach(async () => {
     await render(AppComponent);
+
+    inputBar = screen.getByRole('textbox', {
+      name: /name/i,
+    });
+    confirmButton = screen.getByRole('button', {
+      name: /confirm/i,
+    });
+  });
+
+  test('error modal is displayed if you click on "Confirm" without inputing a name', async () => {
+    fireEvent.click(confirmButton);
+    expect(screen.getByText(/you must enter a /i)).toBeInTheDocument();
   });
 
   test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
+    await userEvent.type(inputBar, 'Name');
+    fireEvent.click(confirmButton);
+
+    const modalCancelButton = screen.getByRole('button', {
+      name: /cancel/i,
+    });
+
+    fireEvent.click(modalCancelButton);
+
+    expect(screen.getByText(/Name is invalid !!/i)).toBeInTheDocument();
   });
 
   test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
+    await userEvent.type(inputBar, 'Name');
+    fireEvent.click(confirmButton);
+
+    const modalConfirmButton = screen.getByRole('button', {
+      name: /confirmation/i,
+    });
+
+    fireEvent.click(modalConfirmButton);
+
+    expect(screen.getByText(/Name has been submitted/i)).toBeInTheDocument();
   });
 });
