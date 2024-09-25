236c0c980e7151ed22bd85a78510881120afd332
Romain Baselga <romain.baselga@gmail.com>
2024-09-25 11:28:33 +0200

    feat: implement test and harness

--- apps/testing/24-harness-creation/src/app/app.component.spec.ts
+++ apps/testing/24-harness-creation/src/app/app.component.spec.ts
@@ -1,22 +1,45 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { render } from '@testing-library/angular';
 import { AppComponent } from './app.component';
+import { MySliderHarness } from './slider.harness';
 
 describe('AppComponent', () => {
+  let slider1: MySliderHarness;
+  let slider2: MySliderHarness;
+  let loader: HarnessLoader;
+
+  beforeEach(async () => {
+    const result = await render(AppComponent);
+    loader = TestbedHarnessEnvironment.loader(result.fixture);
+    [slider1, slider2] = await loader.getAllHarnesses(MySliderHarness);
+  });
+
   describe('When clicking 2 times on plus button of first slider', () => {
     test('Then value is 16', async () => {
-      await render(AppComponent);
+      await slider1.clickPlus();
+      await slider1.clickPlus();
+      expect(await slider1.getValue()).toEqual(16);
     });
   });
 
   describe('When clicking 1 time on plus button and two times on minus button of first slider', () => {
     test('Then value is still 10', async () => {
-      await render(AppComponent);
+      await slider1.clickPlus();
+      await slider1.clickMinus();
+      expect(await slider1.getValue()).toEqual(10);
     });
   });
 
   describe('When clicking 4 times on plus button of slider 1', () => {
     test('Then slider 2 is enabled', async () => {
-      await render(AppComponent);
+      await slider1.clickPlus();
+      await slider1.clickPlus();
+      await slider1.clickPlus();
+      await slider1.clickPlus();
+
+      expect(await slider1.getValue()).toBeGreaterThan(20);
+      expect(await slider2.isEnabled()).toBe(true);
     });
   });
 });
--- apps/testing/24-harness-creation/src/app/slider.harness.ts
+++ apps/testing/24-harness-creation/src/app/slider.harness.ts
@@ -1,5 +1,39 @@
 import { ComponentHarness } from '@angular/cdk/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatSliderThumbHarness } from '@angular/material/slider/testing';
 
 export class MySliderHarness extends ComponentHarness {
   static hostSelector = 'app-slider';
+
+  private getPlusButton = this.locatorFor(
+    MatButtonHarness.with({ selector: '#plusButton' }),
+  );
+  private getMinusButton = this.locatorFor(
+    MatButtonHarness.with({ selector: '#minusButton' }),
+  );
+
+  private getSliderThumb = this.locatorFor(MatSliderThumbHarness);
+
+  async clickPlus(): Promise<void> {
+    return (await this.getPlusButton()).click();
+  }
+
+  async clickMinus(): Promise<void> {
+    return (await this.getMinusButton()).click();
+  }
+
+  async setValue(value: number): Promise<void> {
+    return (await this.getSliderThumb()).setValue(value);
+  }
+  async getValue(): Promise<number> {
+    return (await this.getSliderThumb()).getValue();
+  }
+
+  async isDisabled(): Promise<boolean> {
+    return (await this.getSliderThumb()).isDisabled();
+  }
+
+  async isEnabled(): Promise<boolean> {
+    return !(await this.getSliderThumb()).isDisabled();
+  }
 }
